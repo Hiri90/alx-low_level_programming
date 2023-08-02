206d51cc21b21c991c65ac3d0ea211ddc1f6f541
Hiri90 <anthonyhirikinuthia@gmail.com>
2023-08-02 01:37:02 -0700

    Solution to task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,8 +6,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	
+	int c;
+
+	if (*s == '\0')
 		return (0);
-	else 
-		return (1 + _strlen_recursion(s + 1));
+
+	c = 1 +  _strlen_recursion(s + 1);
+
+	return (c);
 }
