57a25aa7eff38d458de5a5fbaea984d03b8a43af
Hiri90 <anthonyhirikinuthia@gmail.com>
2023-08-17 03:35:05 -0700

    Solution to task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of passed unsigned integer parameters
+ * @n: integer parameter to be passed
+ * Return: Always 0  
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_list;
+	unsigned int count;
+	int sum;
+
+	if (n == 0)
+		return (0);
+	
+	sum = 0;
+
+	va_start(my_list, n);
+
+	for (count = 0; count < n; count++)
+		sum += va_arg(my_list, int);
+	va_end(my_list);
+
+	return (sum);
+}
