7c287ff0c18c6d76132378e749d52c0883815ac0
Hiri90 <anthonyhirikinuthia@gmail.com>
2023-08-29 13:37:59 +0000

    solution to task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	const listint_t *indicator *h;
-	size_t count = 0;
+
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		count++;
-		indicator = indicator->next;
+		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
+
